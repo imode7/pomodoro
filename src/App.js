160ea21fafa3fa2c65deb6fca64df74efdfc6f9c
PC-10121024\hoon <imode7@naver.com>
2022-06-20 15:07:10 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,84 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import useInterval from "./useInterval";
+import { useState } from "react";
+import moment from "moment";
 
 function App() {
+  const [second, setSecond] = useState(0);
+  const [minute, setMinute] = useState(25);
+  const [rapArray, setRapArray] = useState([]);
+  const [runningTime, setRunningTime] = useState(false);
+
+  useInterval(
+    () => {
+      if (second === 0) {
+        setSecond(60);
+        setMinute(() => minute - 1);
+      }
+      setSecond((second) => second - 1);
+      if (minute === 0 && second === 0) {
+        alert("Times up! Rest 5minutes! you good!");
+        handleRestart();
+        setRapArray([
+          ...rapArray,
+          {
+            rap: rapArray.length + 1,
+            time: moment().format("HH:mm:ss"),
+          },
+        ]);
+      }
+    },
+    runningTime ? 10 : null
+  );
+
+  const handleStartStop = () => {
+    setRunningTime(!runningTime);
+  };
+
+  const handleRestart = () => {
+    setRunningTime(false);
+    setSecond(0);
+    setMinute(25);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <>
+      <header
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <img src="/images/pomodoro.webp" alt="" style={{ width: "1%" }} />
+        <span>뽀모도로 ver 1.0</span>
       </header>
-    </div>
+      <div>
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+          }}
+        >
+          <button onClick={handleStartStop}>start / stop</button>
+          <button onClick={handleRestart}>restart</button>
+        </div>
+        <div style={{ display: "flex", justifyContent: "center" }}>
+          {minute < 10 ? `0${minute}` : minute}:
+          {second < 10 ? `0${second}` : second}
+        </div>
+        {rapArray.map((elem, idx) => {
+          return (
+            <div
+              key={idx}
+              style={{ display: "flex", justifyContent: "center" }}
+            >
+              회차: {elem.rap} / 시간: {elem.time}
+            </div>
+          );
+        })}
+      </div>
+    </>
   );
 }
 
